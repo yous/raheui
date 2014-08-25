b9df380d23234d636486cf60fc0123a62304aca2
ChaYoung You <yousbe@gmail.com>
2014-08-25 18:57:27 +0900

    Add Runner class
    밣밞따빠싹밝타맣밦빠다밞따빠빠맣맣밤밣따빠싺뭏
    붒뻐멓터벍뻐멓멓뻐더벅뻐멓더벎뻐서멓더더뻐벌석
    다맣삮맣사빠밞타맣빠맣빠발밦다타맣밝다빠맣맣희

---
+++ lib/raheui/runner.rb
@@ -0,0 +1,212 @@
+# encoding: utf-8
+module Raheui
+  # Run Aheui code.
+  class Runner
+    # Point class for execution cursor position and delta of it.
+    Point = Struct.new(:x, :y)
+
+    # Numbers of required elements for each command.
+    REQUIRED_STORE_SIZE = [
+      0, 0, 2, 2, 2, 2, 1, 0, 1, 0, # ㄱ ㄲ ㄴ ㄷ ㄸ ㄹ ㅁ ㅂ ㅃ ㅅ
+      1, 0, 2, 0, 1, 0, 2, 2, 0     # ㅆ ㅇ ㅈ ㅉ ㅊ ㅋ ㅌ ㅍ ㅎ
+    ]
+
+    # Delta values of each medial consonant.
+    MEDIAL_DELTAS = [
+      [1, 0], nil, [2, 0], nil,        # ㅏ ㅐ ㅑ ㅒ
+      [-1, 0], nil, [-2, 0], nil,      # ㅓ ㅔ ㅕ ㅖ
+      [0, -1], nil, nil, nil, [0, -2], # ㅗ ㅘ ㅙ ㅚ ㅛ
+      [0, 1], nil, nil, nil, [0, 2],   # ㅜ ㅝ ㅞ ㅟ ㅠ
+      [:+, :-], [:-, :-], [:-, :+]     # ㅡ ㅢ ㅣ
+    ]
+
+    # Numbers of strokes of each final consonant.
+    FINAL_STROKES = [
+      0,                            # No final consonant.
+      2, 4, 4, 2, 5, 5, 3, 5, 7, 9, # ㄱ ㄲ ㄳ ㄴ ㄵ ㄶ ㄷ ㄹ ㄺ ㄻ
+      9, 7, 9, 9, 8, 4, 4, 6, 2, 4, # ㄼ ㄽ ㄾ ㄿ ㅀ ㅁ ㅂ ㅄ ㅅ ㅆ
+      1, 3, 4, 3, 4, 4, 3           # ㅇ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ
+    ]
+
+    private_constant :REQUIRED_STORE_SIZE, :MEDIAL_DELTAS, :FINAL_STROKES
+
+    # Initialize a Runner. Get a Code instance and initialize Stores.
+    #
+    # code - The Code instance to execute.
+    def initialize(code)
+      @code = code
+    end
+
+    # Run the Aheui Code.
+    #
+    # Returns the Integer exit code.
+    def run
+      reset
+      step until @finished
+      @selected_store.pop || 0
+    end
+
+    private
+
+    # Reset cursor and Stores. Select first Store.
+    #
+    # Returns nothing.
+    def reset
+      @cursor = Point.new(0, 0)
+      @delta = Point.new(0, 1)
+      @stores = Code::FINAL_CONSONANTS.times.map do |consonant|
+        case consonant
+        when 21 then Queue.new # ㅇ
+        when 27 then Port.new  # ㅎ
+        else         Stack.new
+        end
+      end
+      @selected_store = @stores[0]
+      @finished = false
+    end
+
+    # Process current character which cursor points to and move cursor.
+    #
+    # Returns nothing.
+    def step
+      consonants = @code[@cursor.x, @cursor.y]
+      unless consonants.empty?
+        initial, medial, final = consonants
+        @delta.x, @delta.y = delta(medial)
+        if @selected_store.size < REQUIRED_STORE_SIZE[initial]
+          @delta.x, @delta.y = -@delta.x, -@delta.y
+        else
+          process(initial, final)
+        end
+      end
+      move
+    end
+
+    # Process a Korean alphabet.
+    #
+    # initial - An Integer index of initial consonant of the Korean alphabet.
+    # final   - An Integer index of final consonant of the Korean alphabet.
+    #
+    # Returns nothing.
+    def process(initial, final)
+      case initial
+      when 2 # ㄴ
+        operate(:/)
+      when 3 # ㄷ
+        operate(:+)
+      when 4 # ㄸ
+        operate(:*)
+      when 5 # ㄹ
+        operate(:%)
+      when 6 # ㅁ
+        op = @selected_store.pop
+        if final == 21 # ㅇ
+          IO.print_int(op)
+        elsif final == 27 # ㅎ
+          IO.print_chr(op)
+        end
+      when 7 # ㅂ
+        op = if final == 21 # ㅇ
+               IO.read_int
+             elsif final == 27 # ㅎ
+               IO.read_chr
+             else
+               FINAL_STROKES[final]
+             end
+        @selected_store.push(op)
+      when 8 # ㅃ
+        @selected_store.push_dup
+      when 9 # ㅅ
+        @selected_store = @stores[final]
+      when 10 # ㅆ
+        op = @selected_store.pop
+        @stores[final].push(op)
+      when 12 # ㅈ
+        op1 = @selected_store.pop
+        op2 = @selected_store.pop
+        @selected_store.push(op2 >= op1 ? 1 : 0)
+      when 14 # ㅊ
+        op = @selected_store.pop
+        @delta.x, @delta.y = -@delta.x, -@delta.y if op == 0
+      when 16 # ㅌ
+        operate(:-)
+      when 17 # ㅍ
+        @selected_store.swap
+      when 18 # ㅎ
+        @finished = true
+      end
+    end
+
+    # Helper method for basic operators. +, -, *, / and % can be processed.
+    #
+    # method - A Symbol method to execute.
+    #
+    # Examples
+    #
+    #   operate(:+)
+    #
+    #   operate(:-)
+    #
+    #   operate(:*)
+    #
+    #   operate(:/)
+    #
+    #   operate(:%)
+    #
+    # Returns nothing.
+    def operate(method)
+      op1 = @selected_store.pop
+      op2 = @selected_store.pop
+      @selected_store.push([op2, op1].reduce(method))
+    end
+
+    # Get delta x and y position for next move.
+    #
+    # medial - An Integer index of medial consonant of the Korean alphabet.
+    #
+    # Examples
+    #
+    #   delta(0)
+    #   # => [1, 0]
+    #
+    # Returns an Array of Integer delta x and y position.
+    def delta(medial)
+      delta = MEDIAL_DELTAS[medial]
+      if delta
+        x, y = delta
+        x = x == :+ ? @delta.x : -@delta.x if x.is_a? Symbol
+        y = y == :+ ? @delta.y : -@delta.y if y.is_a? Symbol
+        [x, y]
+      else
+        [@delta.x, @delta.y]
+      end
+    end
+
+    # Move cursor to proper position. Wrap the position if it goes to outside of
+    # the code.
+    #
+    # Returns nothing.
+    def move
+      @cursor.x = wrap(@cursor.x + @delta.x, @code.width)
+      @cursor.y = wrap(@cursor.y + @delta.y, @code.height)
+    end
+
+    # Wrap a number to be between 0 and max value excluding max value. If the
+    # number is negative, it goes to max - 1. If the number is bigger than or
+    # equal to max value, it goes to zero.
+    #
+    # num - An Integer to be wrapped.
+    # max - An Integer max value.
+    #
+    # Returns an Integer between 0 and max - 1.
+    def wrap(num, max)
+      if num < 0
+        max - 1
+      elsif num >= max
+        0
+      else
+        num
+      end
+    end
+  end
+end
---
+++ spec/raheui/runner_spec.rb
@@ -0,0 +1,464 @@
+# encoding: utf-8
+require 'spec_helper'
+
+shared_examples 'a runner' do
+  subject(:runner) { Raheui::Runner.new(code) }
+  let(:code) { Raheui::Code.new(source) }
+  let(:output) { $stdout.string }
+
+  before(:example) { $stdout = StringIO.new }
+  after(:example) { $stdout = STDOUT }
+
+  it 'returns exit code and prints expected output' do
+    expect(runner.run).to be(exit_code)
+    expect(output).to eq(result)
+  end
+end
+
+describe Raheui::Runner, 'with bieup.aheui' do
+  let(:source) { <<-END }
+숭
+박반받발밤밥밧밪밫밬밭붚
+뭉멍멍멍멍멍멍멍멍멍멍멍
+밖밗밙밚밝밞밟밠밡밢밣밦붔
+뭉멍멍멍멍멍멍멍멍멍멍멍멍
+희
+  END
+
+  let(:result) { '2235442343444455799799864' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with border.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+볻         망볿
+ㅇ         ㅇ희
+멍         붒
+ㅇ         ㅇ몽
+
+
+                  표준: 코드 공간의 한 쪽 끝에 다다르면 커서는 반대편 끝으로 이동합니다. 이는 Funge-98의 wrapping algorithm과 동일합니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '369' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with chieut.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+아ㅇㅇ부
+희멍번처붇
+ㅇㅇㅇ분멍
+희멍번차붇
+ㅇㅇㅇ희멍
+ㅊ은 조건 명령으로 저장공간에서 값 하나를 뽑아내서 그 값이 0이 아니면 홀소리의 방향대로, 0이면 그 반대 방향대로 갑니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '33' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with default-storage.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+밞산바삳바사망희
+
+처음에 선택되어 있는 스택은 (받침 없음) 스택입니다. (“사” 와 같은 명령으로 선택할 수 있습니다.)
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '9' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with digeut.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+반받다망희
+
+ㄷ은 덧셈 명령으로 저장공간에서 두 값을 뽑아낸 다음 둘을 더한 값을 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '5' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with exhausted-storage.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+아ㅇㅇ우
+희멍벋망반망희
+중복 명령을 포함한 모든 뽑아내기를 쓰는 명령에서 저장 공간에 값이 모자랄 경우, 커서는 그 명령을 실행하지 않고 커서가 있는 글자의 홀소리의 반대방향으로 움직입니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '3' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with exitcode.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+반월회
+
+ㅎ은 끝냄 명령으로 커서의 실행을 끝냅니다. 이 때 스택에서 맨 위 값을 뽑아서 운영체제에 돌려 줍니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '' }
+  let(:exit_code) { 2 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with ieunghiheuit.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+아악안앋압알앗았앜앇헐
+
+ㅇ은 없음 명령으로 아무 일도 하지 않습니다. * ㅎ은 끝냄 명령으로 커서의 실행을 끝냅니다.
+사용되지 않는 받침은 모두 무시됩니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with jieut.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+반반자망받반자망반받자망희
+
+ㅈ은 비교 명령으로 저장공간에서 값 두 개를 뽑아 내서 비교합니다. 나중에 뽑아낸 값이 더 크거나 같으면 1을, 아니면 0을 지금 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '110' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with mieum.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+바반받밤발밦밠밣밞망만맘말망맋맠맟망희
+
+ㅁ에 ㅇ받침이 있으면 저장공간에서 뽑아낸 값을 숫자로, ㅎ받침이 있으면 그 값에 해당하는 유니코드 문자로 출력합니다. 나머지 받침은 뽑아낸 값을 그냥 버립니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '950' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with nieun.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+밟받나망희
+
+ㄴ은 나눗셈 명령으로 저장공간에서 두 값을 뽑아낸 다음 나중 값에서 먼저 값을 나눈 값을 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '3' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with pieup.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+바밟밟땅밝밝땅팡망망우
+숭ㅇㅇㅇㅇㅇㅇㅇㅇㅇ어
+밟밟밝밝땅땅바팡망망희
+
+ㅍ은 바꿔치기 명령입니다. 지금 저장공간이 스택이라면 맨 위 값과 그 바로 아래 값을, 큐라면 맨 앞의 값과 그 바로 뒤 값을 바꿉니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '81494981' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with print.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+밞밞반다따반타뭉
+ㅇㅇㅇㅇㅇㅇㅇ밞밞반다따반타맣희
+
+ㅁ에 ㅇ받침이 있으면 저장공간에서 뽑아낸 값을 숫자로, ㅎ받침이 있으면 그 값에 해당하는 유니코드 문자로 출력합니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '97a' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with rieul.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+밟발라망희
+
+ㄹ은 나머지 명령으로 저장공간에서 두 값을 뽑아낸 다음 나중 값에서 먼저 값을 나눈 나머지를 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '4' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with shebang.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+#!/usr/bin/env aheui
+반망희
+
+표준: 커서는 코드 공간의 맨 첫 줄 맨 첫번째 칸에서 시작합니다. 맨 처음에 홀소리가 없을 경우 커서는 기본값으로 아랫쪽으로 이동하는데, 이는 #!과 호환시키기 위한 것으로, 기본 방향이 오른쪽인 funge와는 다른 점입니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '2' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with ssangbieup.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+밟밟땅빵망망희
+
+ㅃ은 중복 명령입니다. 지금 저장공간이 스택이라면 맨 위의 값을 그 값 위에 하나 더 집어넣고, 큐라면 맨 앞의 값을 앞에 하나 더 덧붙입니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '8181' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with ssangdigeut.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+발밞따망희
+
+ㄸ은 곱셈 명령으로 저장공간에서 두 값을 뽑아낸 다음 둘을 곱한 값을 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '45' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with storage.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+바반받뭉
+우석멍멍
+바반받뭉
+우선멍멍
+바반받뭉
+우섣멍멍
+바반받뭉
+우설멍멍
+바반받뭉
+우섬멍멍
+바반받뭉
+우섭멍멍
+바반받뭉
+우섯멍멍
+바반받뭉
+우성멍멍
+바반받뭉
+우섲멍멍
+바반받뭉
+우섳멍멍
+바반받뭉
+우섴멍멍
+바반받뭉
+우섵멍멍
+바반받뭉
+우섶멍멍
+바반받뭉
+끝희멍멍
+
+아희에서 쓰이는 저장 공간은 여러 개가 있습니다. 대부분이 스택이고, 큐가 하나 있습니다.
+ㅁ은 뽑기 명령입니다. 지금 저장공간이 스택이라면 맨 위의 값, 큐라면 맨 앞의 값을 뽑아냅니다.(pop) 통로라면 거기서 값을 하나 뽑아옵니다. ㅁ 명령에는 받침이 올 수 있는데, 이에 대한 설명은 아래에 있습니다.
+ㅁ에 ㅇ받침이 있으면 저장공간에서 뽑아낸 값을 숫자로, ㅎ받침이 있으면 그 값에 해당하는 유니코드 문자로 출력합니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '320320320320320320320320023320320320320320' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with syllable.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+ㅏ희ㅣㅊㅓ
+뱓ㅗㅈㅊ몽
+ㅂㅁㅎㅣ헥
+
+코드는 한글로만 이루어지는데, 여기서 한글은 유니코드 U+AC00에서 U+D7A3까지의 범위에 있는 글자(hangul syllable 영역)를 말합니다. 그 밖의 글자나 코드에서 지정하지 않은 나머지 공간은 모두 빈 칸으로 처리되어 커서가 이동하는 방향에 아무 영향을 주지 않습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '3' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with tieut.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+받반타망희
+
+ㅌ은 뺄셈 명령으로 저장공간에서 두 값을 뽑아낸 다음 나중 값에서 먼저 값을 뺀 값을 저장공간에 집어넣습니다.
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '1' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with vowel-2step.aheui' do
+  let(:source) { <<-END }
+뷷우우우희우명
+아아아아아아아
+먕오뱞오먕오뵯
+
+ㅑ, ㅕ, ㅛ, ㅠ - 커서를 각각 오른쪽, 왼쪽, 위, 아래로 두 칸 옮깁니다.
+  END
+
+  let(:result) { '369' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with vowel-advanced.aheui' do
+  let(:source) { <<-END }
+반븓븝불
+우멍벎망이밟망희
+빈
+빋밟망희
+붑으
+발몽
+ㅇ밞망분
+ㅇ불법벋
+의멍밞망희
+
+ㅡ - 커서가 가로로 왔으면 그 방향대로, 세로로 왔으면 전에 있던 자리로 옮깁니다.
+ㅣ - 커서가 세로로 왔으면 그 방향대로, 가로로 왔으면 전에 있던 자리로 옮깁니다.
+ㅢ - 커서를 전에 있던 자리로 옮깁니다.
+  END
+
+  let(:result) { '543295432954329' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with vowel-basic.aheui' do
+  let(:source) { <<-END }
+붇희희멍
+망밦망볿
+
+ㅏ, ㅓ, ㅗ, ㅜ - 커서를 각각 오른쪽, 왼쪽, 위, 아래로 한 칸 옮깁니다.
+  END
+
+  let(:result) { '369' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with vowel-useless.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+우아앙배벤뱯볩뷜뫙뫵뮝뭥뮁우
+배맹희맹멩먱몡뮝봘봽붣붠붸어
+벤멩
+뱯먱
+볩몡
+뷜뮝
+뫙봘
+뫵봽
+묑뵏
+뭥붠
+뮁붸
+아오
+
+기능 없음: ㅐ ㅔ ㅒ ㅖ ㅘ ㅙ ㅚ ㅝ ㅞ ㅟ (커서가 이동하는 방향은 변하지 않습니다.)
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '54320543205432054320' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
+
+describe Raheui::Runner, 'with vowel-useless2.aheui' do
+  # rubocop:disable Metrics/LineLength
+  let(:source) { <<-END }
+와아앙배벤뱯볩뷜뫙뫵뮝뭥뮁우
+배맹희맹멩먱몡뮝봘봽붣붠붸어
+벤멩
+뱯먱
+볩몡
+뷜뮝
+뫙봘
+뫵봽
+묑뵏
+뭥붠
+뮁붸
+아오
+
+기능 없음: ㅐ ㅔ ㅒ ㅖ ㅘ ㅙ ㅚ ㅝ ㅞ ㅟ (커서가 이동하는 방향은 변하지 않습니다.)
+  END
+  # rubocop:enable Metrics/LineLength
+
+  let(:result) { '54320543205432054320' }
+  let(:exit_code) { 0 }
+
+  it_behaves_like 'a runner'
+end
