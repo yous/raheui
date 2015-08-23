1fc832641001c4c05446879852b690aaa2123d43
Chayoung You <yousbe@gmail.com>
2015-08-23 10:16:55 +0900

    Reimplement Store

--- lib/raheui/store.rb
+++ lib/raheui/store.rb
@@ -8,32 +8,40 @@ module Raheui
   class Store
     extend Forwardable
 
-    BASE_METHODS = [:push, :pop, :push_dup, :swap]
-    private_constant :BASE_METHODS
-
     # Delegates size to @store.
     def_delegator :@store, :size
 
     # Initialize a Stack.
     def initialize
-      check_base_methods
       @store = []
     end
 
-    private
-
-    # Check whether base methods are implemented.
+    # Base method push.
     #
-    # Returns nothing.
-    # Raises NotImplementedError if base methods are not implemented.
-    def check_base_methods
-      errors = []
-      BASE_METHODS.each do |method|
-        errors << method unless respond_to?(method)
-      end
-      return if errors.empty?
-      fail NotImplementedError, 'base methods are not implemented:' \
-                                " #{errors.join(', ')}"
+    # Raises NotImplementedError.
+    def push
+      fail NotImplementedError, '#push is not implemented'
+    end
+
+    # Base method pop.
+    #
+    # Raises NotImplementedError.
+    def pop
+      fail NotImplementedError, '#pop is not implemented'
+    end
+
+    # Base method push_dup.
+    #
+    # Raises NotImplementedError.
+    def push_dup
+      fail NotImplementedError, '#push_dup is not implemented'
+    end
+
+    # Base method swap.
+    #
+    # Raises NotImplementedError.
+    def swap
+      fail NotImplementedError, '#swap is not implemented'
     end
   end
 end
--- spec/raheui/store_spec.rb
+++ spec/raheui/store_spec.rb
@@ -1,103 +1,26 @@
 # encoding: utf-8
 
 describe Raheui::Store do
-  let(:base_methods) { Raheui::Store.const_get(:BASE_METHODS) }
-  let(:error_msg) { 'base methods are not implemented: %s' }
+  let(:error_msg) { '#%s is not implemented' }
 
   describe '#initialize' do
-    it 'raises NotImplementedError' do
-      expect { subject }
-        .to raise_error(NotImplementedError,
-                        format(error_msg, base_methods.join(', ')))
-    end
-
-    context 'with implemented push' do
-      let(:methods) { base_methods - [:push] }
-
-      before(:context) { Raheui::Store.class_eval { def push; end } }
-      after(:context) { Raheui::Store.class_eval { remove_method :push } }
-
-      it 'raises NotImplementedError' do
-        expect(base_methods - methods).to contain_exactly(:push)
-        expect { subject }
-          .to raise_error(NotImplementedError,
-                          format(error_msg, methods.join(', ')))
-      end
-    end
-
-    context 'with implemented pop' do
-      let(:methods) { base_methods - [:pop] }
-
-      before(:context) { Raheui::Store.class_eval { def pop; end } }
-      after(:context) { Raheui::Store.class_eval { remove_method :pop } }
-
-      it 'raises NotImplementedError' do
-        expect(base_methods - methods).to contain_exactly(:pop)
-        expect { subject }
-          .to raise_error(NotImplementedError,
-                          format(error_msg, methods.join(', ')))
-      end
-    end
-
-    context 'with implemented push_dup' do
-      let(:methods) { base_methods - [:push_dup] }
-
-      before(:context) { Raheui::Store.class_eval { def push_dup; end } }
-      after(:context) { Raheui::Store.class_eval { remove_method :push_dup } }
-
-      it 'raises NotImplementedError' do
-        expect(base_methods - methods).to contain_exactly(:push_dup)
-        expect { subject }
-          .to raise_error(NotImplementedError,
-                          format(error_msg, methods.join(', ')))
-      end
-    end
-
-    context 'with implemented swap' do
-      let(:methods) { base_methods - [:swap] }
-
-      before(:context) { Raheui::Store.class_eval { def swap; end } }
-      after(:context) { Raheui::Store.class_eval { remove_method :swap } }
-
-      it 'raises NotImplementedError' do
-        expect(base_methods - methods).to contain_exactly(:swap)
-        expect { subject }
-          .to raise_error(NotImplementedError,
-                          format(error_msg, methods.join(', ')))
-      end
-    end
-
-    context 'with implemented all base methods' do
-      before(:context) do
-        Raheui::Store.class_eval do
-          def push; end
-
-          def pop; end
-
-          def push_dup; end
-
-          def swap; end
-        end
-      end
-
-      after(:context) do
-        Raheui::Store.class_eval do
-          remove_method :push
-          remove_method :pop
-          remove_method :push_dup
-          remove_method :swap
-        end
-      end
-
-      it 'does not raise NotImplementedError' do
-        expect { subject }.not_to raise_error
-      end
+    it 'initializes @store' do
+      expect(subject.instance_variable_get(:@store)).to eq([])
     end
   end
 
   describe '#size' do
-    it 'should respond to #size' do
-      expect(Raheui::Store.instance_methods(false)).to include(:size)
+    it 'responds to #size' do
+      expect(subject).to respond_to(:size)
+    end
+  end
+
+  %w(push pop push_dup swap).each do |base_method|
+    describe "##{base_method}" do
+      it 'raises NotImplementedError' do
+        expect { subject.send(base_method.to_sym) }
+          .to raise_error(NotImplementedError, format(error_msg, base_method))
+      end
     end
   end
 end
